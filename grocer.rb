cd4ecef929444b559d684f6ba8f618a6273eb2d9
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-30 23:54:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -73,7 +73,7 @@ def checkout(cart, coupons)
   total = 0 
   
     con_cart_coupon.each do |item|
-      #total += item[1][:price] * item[1][:count]
+      total += item[1][:price] * item[1][:count]
       binding.pry
     end
   total 
