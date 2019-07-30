ca40bd0d68d705e06b6ad3e42af27d514ac174d9
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-30 23:35:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,7 +70,7 @@ def checkout(cart, coupons)
   con_cart_clearance = apply_clearance(con_cart)
   con_cart_coupon = apply_coupons(con_cart_clearance, coupons)
   
-    con_cart_coupon.reduce do | value|
+    con_cart_coupon.reduce do |total, value|
       num = value[1][:price] 
       total = total + num
       binding.pry
